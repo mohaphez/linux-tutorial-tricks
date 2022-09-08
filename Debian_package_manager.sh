72d290d6cd2d5759a69396cbc12ce0d0224a58b3
mohaphez <mohaphez@yahoo.com>
2022-09-08 13:38:31 +0430

    edit rpm_package_manager and add debian_package_manager

---
+++ Debian_package_manager.sh
@@ -0,0 +1,46 @@
+# #### Here we'll learn how manage packages on debian based distributions  ####
+
+# Some of the debian based distributions: 
+    # Mint
+    # Ubuntu
+    # Deepin
+    # Kali Linux 
+    # Parrot OS
+    # MX
+    # AntiX
+
+# Download deb files with apt utility 
+    apt download emacs 
+     # This utility download the emacs DEB file to your current working directory 
+    ls -l *.deb 
+     # check DEB file downloaded successfully .
+    
+# The dpkg is utility lets you install , modify and remove software packages . 
+# DEB package files have an .deb file extention and follow this naming format :
+    # PACKAGE-NAME-VERSION-RELEASE.ARCHITECTURE.deb
+        # PACKAGE-NAME : the name of the software package. expamle : sublime
+        # VERSION : it's the program's version number and represents modification that are more recent than older version numbers
+        # RELEASE: it's also called build number . it represents a smaller program modification than does the version number
+        # ARCHITECTURE: This is a designation of the CPU architecture for which the software package was optimized . 
+    dpkg -i emacs*.deb      # install the deb file on your system . 
+    dpkg -C emacs*.deb      # searches for broken installed packages and suggests how to fix them.
+    dpkg --configure emacs  # Reconfigures an installed package 
+    dpkg -I docker*.deb     # Displays information about and uninstalled packagefile 
+    dpkg -l                 # Lists all installed packages matching a specified pattern 
+    dpkg -p emacs           # Displays information about an installed 
+    dpkg -P emacs           # Removes an installed package , including configuration files
+    dpkg -r emacs           # Removes an installed package but leaves the configuration files 
+    dpkg -s emacs           # Displays the status of the specified package.
+
+# apt (advance package tool) is a repository tools and debian package manager 
+# it's allows you to install and remove software packages on Redhat based distributions
+    # apt command use the /etc/apt/sources.list file to indentify the locations of where to look for repositories
+    apt autoclean         # Removes information about packages that are no longer in the repository
+    apt clean             # Remove temporary files downloaded during install 
+    apt install nano      # Installs and updates the specified package . In this case is nano 
+    apt remove nano       # Removes a package from the system 
+    apt update            # Retrieves updated information about packages in the repository
+    apt upgrade           # Updates all installed packages to newest version 
+
+# apt-cache is a program that providers information about the package database. 
+  
--- RPM_package_manager.sh
+++ RPM_package_manager.sh
@@ -34,3 +34,28 @@
     rpm -qa              # -a option shows all installed rpm packages on your system.
     rpm -qc  emacs       # -c option shows package configuration filenames and paths that belongs to a package 
  
+# Yum (YellowDog Update Manager) is a repository tools and rpm package manager 
+# it's allows you to query ,install and remove software packages on Redhat based distributions
+    # yum command use the /etc/yum.repos.d/ directory to hold files that list the different repositories it checks for packages . 
+    ls /etc/yum.repos.d/  # viewing the repository files on a CentOS distro
+    yum check-update      # Checks the repository for updates to installed packages 
+    yum clean             # Remove temporary files downloaded during install 
+    yum info nano         # Displays information about the specified package. In this case is nano 
+    yum install nano      # Installs the specified package . In this case is nano 
+    yum list              # Displays information about installed packages 
+    yum reinstall nano    # Reinstalls the specified package 
+    yum remove nano       # Removes a package from the system 
+    yum search docker     # Searches repository package names and descriptions for specified keyword. In this case is docker
+    yum update            # Updates the specified packages to the latest version in the repository
+    yum upgrade           # Updates specified packages but removes obsolete packages. 
+
+### The dnf (dandified yum) is included as part of the Fedora Linux distribution as a replacement for yum
+### dnf provides some advanced features that yum is missing . 
+
+# The openSUSE Linux distribution uses the RPM package management system and distributes software in .rpm files but doesn't use the yum or dnf tool. Instead , Its use own package management tool and called ZYpp 
+    # zypper allows you to query ,install and remove software packages on opensuse distribution
+    zypper info nano         # Displays information about the specified package. In this case is nano 
+    zypper install nano      # Installs the specified package . In this case is nano 
+    zypper remove nano       # Removes a package from the system 
+    zypper search docker     # Searches repository package names and descriptions for specified keyword. In this case is docker
+    zypper update            # Updates the specified packages to the latest version in the repository
